75f4f6b6f28d2d9d01dbeea09f19744dc1ad871b
Alexander Voronische <voronizer@gmail.com>
2023-04-07 12:03:54 +0300

    Initial data structure

--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Transport/Http.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Transport/Http.cs
@@ -1,79 +1,80 @@
 using System;
 using System.Data;
+using System.IO;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
-namespace Polimaster.Device.Abstract.Transport; 
+namespace Polimaster.Device.Abstract.Transport;
 
-public abstract class Http<TData> : ITransport<TData> {
+public abstract class Http : ITransport<string> {
+    private readonly TcpClient _client;
     private readonly string _ip;
     private readonly int _port;
     private readonly int _connectTimeout;
-    private readonly int _readWriteTimeout;
-    private TcpClient? _client;
 
     /// <inheritdoc cref="ITransport{TData}.ConnectionState"/>
-    public virtual ConnectionState ConnectionState {
+    public ConnectionState ConnectionState {
         get {
-            if (_client == null) return ConnectionState.Closed;
             lock (_client) { return _client.Connected ? ConnectionState.Open : ConnectionState.Closed; }
         }
     }
 
     /// <inheritdoc cref="ITransport{TData}.ConnectionStateChanged"/>
-    public virtual event Action<ConnectionState>? ConnectionStateChanged;
+    public event Action<ConnectionState>? ConnectionStateChanged;
 
-    protected Http(string ip, int port, int connectTimeout = 5000, int readWriteTimeout = 5000) {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="client">Transport client</param>
+    /// <param name="ip">IP address to connect</param>
+    /// <param name="port">IP port to connect</param>
+    /// <param name="connectTimeout">Connect timeout</param>
+    protected Http(TcpClient client, string ip, int port, int connectTimeout = 5000) {
+        _client = client;
         _ip = ip;
         _port = port;
         _connectTimeout = connectTimeout;
-        _readWriteTimeout = readWriteTimeout;
-        InitClient();
-    }
-
-    /// <summary>
-    /// Initialize TcpClient
-    /// </summary>
-    protected void InitClient() {
-        lock (_client!) {
-            _client?.Close();
-            _client?.Dispose();
-            _client = new TcpClient();
-            _client.ReceiveTimeout = _readWriteTimeout;
-            _client.SendTimeout = _readWriteTimeout;
-        }
     }
 
     /// <inheritdoc cref="ITransport{TData}.Open"/>
     public virtual Task Open() {
-        lock (_client!) {
-            if (ConnectionState == ConnectionState.Open) return Task.CompletedTask;
-            var connected = _client.ConnectAsync(_ip, _port).Wait(_connectTimeout);
-            if (!connected) throw new TimeoutException($"Connection to {_ip}:{_port} timed out");
-            ConnectionStateChanged?.Invoke(ConnectionState);
-            return Task.CompletedTask;
-        }
+        if (ConnectionState == ConnectionState.Open) return Task.CompletedTask;
+        var connected = _client.ConnectAsync(_ip, _port).Wait(_connectTimeout);
+        if (!connected) throw new TimeoutException($"Connection to {_ip}:{_port} timed out");
+        ConnectionStateChanged?.Invoke(ConnectionState);
+        return Task.CompletedTask;
     }
 
     /// <inheritdoc cref="ITransport{TData}.Close"/>
     public virtual Task Close() {
-        lock (_client!) {
-            InitClient();
-            ConnectionStateChanged?.Invoke(ConnectionState);
-            return Task.CompletedTask;
-        }
+        _client.Close();
+        ConnectionStateChanged?.Invoke(ConnectionState);
+        return Task.CompletedTask;
     }
-    
-    
+
     /// <inheritdoc cref="ITransport{TData}.Write"/>
-    public abstract Task Write(TData command);
+    public virtual Task Write(string command) {
+        using var stream = _client.GetStream();
+        using var writer = new StreamWriter(stream) { AutoFlush = true };
+        writer.WriteLine(command);
+        return Task.CompletedTask;
+    }
 
     /// <inheritdoc cref="ITransport{TData}.Read"/>
-    public abstract Task<TData> Read(TData command);
+    public virtual async Task<string> Read(string command) {
+        await using var stream = _client.GetStream();
+
+        await using var writer = new StreamWriter(stream) { AutoFlush = true };
+        await writer.WriteLineAsync(command);
+
+        using var reader = new StreamReader(stream);
+        var response = await reader.ReadToEndAsync();
+        return response;
+    }
 
     /// <inheritdoc cref="IDisposable.Dispose"/>
     public void Dispose() {
-        _client?.Close();
-        _client?.Dispose();
+        _client.Close();
+        _client.Dispose();
     }
 }
