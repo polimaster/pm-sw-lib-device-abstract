cbef01669bc41a2db29eebf674e28337d30376b2
Alexander Voronische <voronizer@gmail.com>
2023-06-06 13:46:10 +0300

    add DeviceManager interface

---
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/IDeviceManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Polimaster.Device.Abstract;
+
+/// <summary>
+/// Device manager
+/// </summary>
+/// <typeparam name="T">Device type</typeparam>
+public interface IDeviceManager<T> {
+    
+    /// <summary>
+    /// Occurs when irda device connected
+    /// </summary>
+    event Action<T>? Connected;
+    
+    /// <summary>
+    /// Occurs when irda device disconnected
+    /// </summary>
+    event Action<T>? Disconnected;
+    
+    
+    /// <summary>
+    /// Current connected devices
+    /// </summary>
+    List<T> Devices { get; set; }
+    
+    
+    /// <summary>
+    /// Refresh list of device
+    /// </summary>
+    Task RefreshConnectedDevices();
+}
