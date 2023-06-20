e3f9d2a03acd0f0c98833e6005367dc0fab39225
Alexander Voronische <voronizer@gmail.com>
2023-06-20 14:30:50 +0300

    add ICanResetDose

---
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/ICanResetDose.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Polimaster.Device.Abstract.Device; 
+
+/// <summary>
+/// Identifies a device with reset dose function
+/// </summary>
+public interface ICanResetDose {
+    Task ResetDose();
+}
