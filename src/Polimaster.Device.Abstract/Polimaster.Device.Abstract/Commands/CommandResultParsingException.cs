a9d3d1be16db2cae9b2b678f1d987f4c47b70660
Alexander Voronische <voronizer@gmail.com>
2023-06-20 12:13:29 +0300

    add command exceptions

--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Commands/CommandResultParsingException.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Commands/CommandResultParsingException.cs
@@ -5,5 +5,7 @@ namespace Polimaster.Device.Abstract.Commands;
 public class CommandResultParsingException : Exception {
     public CommandResultParsingException(Exception exception) : base("Error while parsing result of command", exception) {
     }
-    
+
+    public CommandResultParsingException(string message) : base(message) {
+    }
 }
