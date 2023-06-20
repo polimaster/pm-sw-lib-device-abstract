68b17405af2ea4aa1b99edf18aa73e72b196add6
Alexander Voronische <voronizer@gmail.com>
2023-06-20 11:29:54 +0300

    add command exceptions

--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract.Tests/Device/MyCommand.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract.Tests/Device/MyCommand.cs
@@ -3,9 +3,9 @@ using Polimaster.Device.Abstract.Commands;
 namespace Polimaster.Device.Abstract.Tests.Device;
 
 public class MyCommand : ICommand<MyParam, string> {
-    public MyParam Param { get; set; }
+    public MyParam? Param { get; set; }
     public string Compile() {
-        return $"{Param.CommandPid} : {Param.Value}";
+        return $"{Param?.CommandPid} : {Param?.Value}";
     }
 
     public void Validate() {
--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract.Tests/Device/MyDevice.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract.Tests/Device/MyDevice.cs
@@ -4,6 +4,6 @@ using Polimaster.Device.Abstract.Transport;
 namespace Polimaster.Device.Abstract.Tests.Device;
 
 public class MyDevice : ADevice<string, string> {
-    public MyDevice(ITransport<string, string> transport) : base(transport) {
+    public MyDevice(ITransport<string, string?> transport) : base(transport) {
     }
 }
