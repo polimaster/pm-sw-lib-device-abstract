c1355bcc5d0b6bd78a82557e401e20d54a44a4c7
Alexander Voronische <voronizer@gmail.com>
2023-07-01 16:32:08 +0300

    minor fix

--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Settings/ADeviceSettings.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Settings/ADeviceSettings.cs
@@ -7,9 +7,9 @@ using Polimaster.Device.Abstract.Device.Settings.Interfaces;
 namespace Polimaster.Device.Abstract.Device.Settings;
 
 public abstract class ADeviceSettings<T> : IDeviceSetting<T>{
-    public abstract ICommand<T>? ReadCommand { get; set; }
-    public abstract ICommand<T>? WriteCommand { get; set; }
-    public abstract T? Value { get; set; }
+    public virtual ICommand<T>? ReadCommand { get; set; }
+    public virtual ICommand<T>? WriteCommand { get; set; }
+    public virtual T? Value { get; set; }
     public bool IsDirty { get; protected set; }
     public bool IsError => Exception != null;
     public Exception? Exception { get; protected set; }
