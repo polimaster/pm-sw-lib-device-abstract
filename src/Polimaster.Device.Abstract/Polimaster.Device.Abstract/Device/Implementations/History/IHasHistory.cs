e93a6bede386f647eb222f494c8ec5f49563b298
Alexander Voronische <voronizer@gmail.com>
2023-07-06 15:50:43 +0300

    add IHistoryReader

---
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Implementations/History/IHasHistory.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Polimaster.Device.Abstract.Device.Settings.Interfaces;
+using Polimaster.Device.Abstract.Transport.Interfaces;
+
+namespace Polimaster.Device.Abstract.Device.Implementations.History;
+
+/// <summary>
+/// Identifies a device can return history
+/// </summary>
+/// <typeparam name="THistory">Type of history record</typeparam>
+/// <typeparam name="TTransport">Data type for <see cref="ITransport{T}"/></typeparam>
+public interface IHasHistory<THistory, TTransport> : IHasClock {
+    
+    /// <summary>
+    /// Interval between history entries
+    /// </summary>
+    IDeviceSetting<ushort?> HistoryInterval { get; }
+
+    /// <summary>
+    /// Wipe history from device
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task WipeHistory(CancellationToken cancellationToken = new());
+    
+    /// <summary>
+    /// <see cref="IHistoryReader{THistory,TTransport}"/>
+    /// </summary>
+    IHistoryReader<THistory, TTransport> HistoryReader { get; set; }
+}
---
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Implementations/History/IHistoryReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Polimaster.Device.Abstract.Device.Interfaces;
+using Polimaster.Device.Abstract.Transport.Interfaces;
+
+namespace Polimaster.Device.Abstract.Device.Implementations.History;
+
+/// <summary>
+/// Device history reader
+/// </summary>
+/// <typeparam name="THistory">Type of history record</typeparam>
+/// <typeparam name="TTransport">Data type for <see cref="ITransport{T}"/></typeparam>
+public interface IHistoryReader<THistory, TTransport> {
+    
+    /// <summary>
+    /// History records available in device
+    /// </summary>
+    int? Available { get; }
+    
+    /// <summary>
+    /// Occurs when new data got from device
+    /// </summary>
+    Action<IEnumerable<THistory>> HasNext { get; set; }
+
+    /// <summary>
+    /// Device <see cref="IHistoryReader{THistory,TTransport}"/> belongs to
+    /// </summary>
+    IDevice<TTransport> Device { get; set; }
+
+    /// <summary>
+    /// Reads history from device
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Array of history records</returns>
+    Task Read(CancellationToken cancellationToken = new());
+}
