01e67241c4d3a206ceb9d5240245ca2b5518fbac
Alexander Voronische <voronizer@gmail.com>
2023-07-06 18:11:49 +0300

    add IHistoryReader

---
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Implementations/History/HistoryChunk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Polimaster.Device.Abstract.Device.Implementations.History;
+
+public struct HistoryChunk<THistory> {
+    /// <summary>
+    /// History records available in device
+    /// </summary>
+    public int? Available;
+
+    /// <summary>
+    /// Indicates if reading process completed
+    /// </summary>
+    public bool Completed;
+
+    /// <summary>
+    /// Retrieved history records
+    /// </summary>
+    public IEnumerable<THistory> Records;
+}
--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Implementations/History/IHistoryReader.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Implementations/History/IHistoryReader.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Polimaster.Device.Abstract.Device.Interfaces;
@@ -13,16 +12,11 @@ namespace Polimaster.Device.Abstract.Device.Implementations.History;
 /// <typeparam name="THistory">Type of history record</typeparam>
 /// <typeparam name="TTransport">Data type for <see cref="ITransport{T}"/></typeparam>
 public interface IHistoryReader<THistory, TTransport> {
-    
-    /// <summary>
-    /// History records available in device
-    /// </summary>
-    int? Available { get; }
-    
+
     /// <summary>
     /// Occurs when new data got from device
     /// </summary>
-    Action<IEnumerable<THistory>> HasNext { get; set; }
+    Action<HistoryChunk<THistory>> HasNext { get; set; }
 
     /// <summary>
     /// Device <see cref="IHistoryReader{THistory,TTransport}"/> belongs to
