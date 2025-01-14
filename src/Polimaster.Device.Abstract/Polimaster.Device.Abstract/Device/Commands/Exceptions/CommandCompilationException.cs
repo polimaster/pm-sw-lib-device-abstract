2baf680c617d3f1faafee6daedf66916fc59c20c
Alexander Voronische <voronizer@gmail.com>
2025-01-14 04:40:14 +0200

    v3.1.0 transient

--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Commands/Exceptions/CommandCompilationException.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Commands/Exceptions/CommandCompilationException.cs
@@ -3,7 +3,7 @@
 namespace Polimaster.Device.Abstract.Device.Commands.Exceptions;
 
 /// <summary>
-/// Rises while result of <see cref="ADataWriter{T,TSteamData}.Compile"/> fails
+/// Rises while result of <see cref="ADataWriter{T}.Compile"/> fails
 /// </summary>
 public class CommandCompilationException : Exception {
     /// <inheritdoc />
--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Commands/Exceptions/CommandResultParsingException.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Commands/Exceptions/CommandResultParsingException.cs
@@ -3,7 +3,7 @@ using System;
 namespace Polimaster.Device.Abstract.Device.Commands.Exceptions;
 
 /// <summary>
-/// Rises while result of <see cref="ADataReader{TValue,TCommand}.Parse"/> fails
+/// Rises while result of <see cref="ADataReader{T}.Parse"/> fails
 /// </summary>
 public class CommandResultParsingException : Exception {
     /// <inheritdoc />
