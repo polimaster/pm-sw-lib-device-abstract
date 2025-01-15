28dea4fa86b52c035989661b491293d98d0c4410
Alexander Voronische <voronizer@gmail.com>
2025-01-15 15:31:25 +0200

    refactor CommandBase

--- src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Commands/CommandBase.cs
+++ src/Polimaster.Device.Abstract/Polimaster.Device.Abstract/Device/Commands/CommandBase.cs
@@ -1,45 +1,24 @@
-﻿using System;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Polimaster.Device.Abstract.Transport;
 
-namespace Polimaster.Device.Abstract.Device.Commands; 
+namespace Polimaster.Device.Abstract.Device.Commands;
 
 /// <summary>
-/// 
+/// Command base class with <see cref="ITransport"/>
 /// </summary>
-public abstract class CommandBase {
+public abstract class CommandBase : ALogged {
 
     /// <summary>
     /// <see cref="ITransport"/> layer for executing command
     /// </summary>
     protected ITransport Transport { get; }
 
-
-    /// <summary>
-    /// Command logger
-    /// </summary>
-    protected ILogger? Logger { get; }
-
     /// <summary>
     /// 
     /// </summary>
     /// <param name="transport"></param>
     /// <param name="loggerFactory"></param>
-    protected CommandBase(ITransport transport, ILoggerFactory? loggerFactory) {
+    protected CommandBase(ITransport transport, ILoggerFactory? loggerFactory) : base(loggerFactory){
         Transport = transport;
-        Logger = loggerFactory?.CreateLogger(GetType());
     }
-    
-    /// <summary>
-    /// Log current command
-    /// </summary>
-    /// <param name="methodName"></param>
-    protected void LogCommand(string methodName) => Logger?.LogDebug("Call {N} with command {C}", methodName, GetType().Name);
-
-    /// <summary>
-    /// Log error
-    /// </summary>
-    /// <param name="e"></param>
-    /// <param name="methodName"></param>
-    protected void LogError(Exception e, string methodName) => Logger?.LogError(e, "Error while sending {N} command {C}",methodName, GetType().Name);
 }
